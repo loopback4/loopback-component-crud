16659d3e067cdd7db2041496a491adebafaff7c3
KoLiBer <koliberr136a1@gmail.com>
2020-06-12 20:29:40 +0430

    clean: remove unused packages openapi, graphql

--- sources/src/keys.ts
+++ sources/src/keys.ts
@@ -2,16 +2,6 @@ import { MetadataAccessor } from "@loopback/context";
 
 import { CRUDMetadata } from "./types";
 
-/**
- * Public bindings used in application scope
- */
-export namespace CRUDBindings {}
-
-/**
- * Private binding used in component scope
- */
-export namespace PrivateCRUDBindings {}
-
 export const CRUD_METHOD_KEY = MetadataAccessor.create<
     CRUDMetadata<any, any, any, any>,
     MethodDecorator
